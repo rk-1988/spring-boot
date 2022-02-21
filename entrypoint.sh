0b5233b98cfe54036cb7c688722e906f943b9825
rk-1988 <99652850+rk-1988@users.noreply.github.com>
2022-02-22 01:58:35 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,9 @@ fi
 cf8 api ${INPUT_API} ${cf_opts}
 CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf8 auth
 cf8 target -o ${INPUT_ORG} -s ${INPUT_SPACE}
-cf8 login -a $INPUT_API -o $INPUT_ORG -s $INPUT_SPACE -u $INPUT_USERNAME -p $INPUT_PASSWORD
-#cf8 push -f ${INPUT_MANIFEST}
+#cf8 login -a $INPUT_API -o $INPUT_ORG -s $INPUT_SPACE -u $INPUT_USERNAME -p $INPUT_PASSWORD
 
 #starting cf8 cli
-sh -c "cf8 $*"   
+sh -c "cf8 $*" 
+
+cf8 push -f ${INPUT_MANIFEST}
