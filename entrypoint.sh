b90ec2fc631ba18db97e01667251f27c40229a5e
rk-1988 <99652850+rk-1988@users.noreply.github.com>
2022-02-23 14:26:56 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/sh -l
 
-set -eu
-cf_opts=
-if ["x${INPUT_VALIDATE}" = "xfalse" ]; then
-  cf_opts="--skip-ssl-validation"
+cf8 api "$INPUT_API"
+cf8 auth "$INPUT_USERNAME" "$INPUT_PASSWORD"
+
+if [ -n "$INPUT_ORG" ] && [ -n "$INPUT_SPACE" ]; then
+  cf8 target -o "$INPUT_ORG" -s "$INPUT_SPACE"
 fi
-cf8 api ${INPUT_API} ${cf_opts}
-CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf8 auth
-cf8 target -o ${INPUT_ORG} -s ${INPUT_SPACE}
-#cf8 login -a $INPUT_API -o $INPUT_ORG -s $INPUT_SPACE -u $INPUT_USERNAME -p $INPUT_PASSWORD
 
-#starting cf8 cli
-sh -c "cf8 $*" 
-
-cf8 push -f ${INPUT_MANIFEST}
+sh -c "cf8 $*"
